29907ef793db41d81f8dcc3c5b604eb30c2b7f8e
Cooper Kelley <kelleyco2@gmail.com>
2022-12-02 23:43:10 -0700

    creates header

--- components/Burger.tsx
+++ components/Burger.tsx
@@ -1,16 +1,33 @@
+import classNames from "classnames";
+
+enum BurgerVariants {
+  PRIMARY = "primary",
+  SECONDARY = "secondary",
+}
+
 type BurgerProps = {
+  variant: BurgerVariants;
   setOpen: (bool: boolean) => void;
 };
 
-const Burger = ({ setOpen }: BurgerProps) => (
-  <div
-    className="flex flex-col justify-around w-8 h-8 p-2 md:hidden"
-    onClick={() => setOpen(true)}
-  >
-    <div className="h-[2px] bg-black" />
-    <div className="h-[2px] bg-black" />
-    <div className="h-[2px] bg-black" />
-  </div>
-);
+const Burger = ({ variant, setOpen }: BurgerProps) => {
+  const isPrimary = variant === BurgerVariants.PRIMARY;
+
+  const sharedClasses = classNames("h-[2px]", {
+    "bg-black": isPrimary,
+    "bg-white": !isPrimary,
+  });
+
+  return (
+    <div
+      className="flex flex-col justify-around w-8 h-8 p-2 md:hidden"
+      onClick={() => setOpen(true)}
+    >
+      <div className={sharedClasses} />
+      <div className={sharedClasses} />
+      <div className={sharedClasses} />
+    </div>
+  );
+};
 
 export default Burger;
