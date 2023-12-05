283e0290766731f1c42fa91a269d24b2bef4320b
Cooper Kelley <kelleyco2@gmail.com>
2023-12-04 19:10:19 -0700

    got hutch ready to rock

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,22 @@
 import { useState } from "react";
-import Button, { ButtonVariants } from "../components/Button";
+import { ButtonVariants } from "../components/Button";
 import FullWidthCta from "../components/FullWidthCta";
-import FullWidthQuote from "../components/FullWidthQuote";
 import { HeaderVariants } from "../components/Header";
-import Input from "../components/Input";
 import Layout from "../components/Layout";
-import Radio from "../components/Radio";
-import Section, { SectionVariants } from "../components/Section";
-import Services from "../components/Services";
-import TwoColumn from "../components/TwoColumn";
 
 export default function Home() {
-  const [value, setValue] = useState("");
   return (
     <Layout headerVariant={HeaderVariants.SECONDARY}>
-      <Section variant={SectionVariants.LARGE}>
-        <Button
-          className="mb-4"
-          href="/"
-          variant={ButtonVariants.PRIMARY}
-          iconRight
-        >
-          Test button
-        </Button>
-        <Input
-          name="test-input"
-          label="Test input"
-          value={value}
-          setValue={setValue}
-        />
-        <Radio className="my-4" name="radio" label="Test radio" />
-        <Radio name="radio" label="Test radio" />
-      </Section>
-      <FullWidthCta subheading="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac a fringilla pharetra, scelerisque tellus. Diam etiam id dolor pretium lectus tempor mi enim ultricies." />
-      <Services />
-      <TwoColumn />
-      <FullWidthQuote quote="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac a fringilla pharetra, scelerisque tellus. Diam etiam id dolor pretium lectus tempor mi enim ultricies." />
+      <FullWidthCta
+        heading="Test heading"
+        subheading="Test subheading"
+        cta={{
+          href: "/",
+          text: "Call to action",
+          variant: ButtonVariants.WHITE,
+        }}
+        hero
+      />
     </Layout>
   );
 }
