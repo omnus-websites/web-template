d3088f1e90784f677a965269da5b3bdfe725ed70
Hutch <hutchrhees@gmail.com>
2023-12-05 18:31:33 -0700

    fixed layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,39 +1,18 @@
 import { ReactNode } from "react";
 import Footer from "./Footer";
 import Header, { HeaderVariants } from "./Header";
-import classNames from "classnames";
 
 type LayoutProps = {
   headerVariant: HeaderVariants;
   children: ReactNode;
-  isHomePage: boolean;
 };
 
-const Layout = ({
-  headerVariant,
-  children,
-  isHomePage = false,
-}: LayoutProps) => {
-  const HomePageBackgroundImage = {
-    backgroundImage: "url('/blue-background-curvy.png')",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  };
-
+const Layout = ({ headerVariant, children }: LayoutProps) => {
   return (
     <>
-      <div
-        className={classNames("", {
-          "bg-primary text-white":
-            headerVariant === HeaderVariants.SECONDARY && !isHomePage,
-        })}
-        style={isHomePage ? HomePageBackgroundImage : {}}
-      >
-        <Header variant={headerVariant} />
-        <main>{children}</main>
-        <Footer />
-      </div>
+      <Header variant={headerVariant} />
+      <main>{children}</main>
+      <Footer />
     </>
   );
 };
