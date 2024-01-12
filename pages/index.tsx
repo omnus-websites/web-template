1cad6ef2102f8df57b8f19b1a44b46785f647859
Hutch <hutchrhees@gmail.com>
2024-01-12 14:54:05 -0700

    added two column to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { HeaderVariants } from "../components/Header";
 import Layout from "../components/Layout";
 import Services from "../components/Services";
 import LogoBar from "../components/LogoBar";
+import TwoColumn from "../components/TwoColumn";
 
 export default function Home() {
   return (
@@ -20,6 +21,17 @@ export default function Home() {
         hero
       />
       <LogoBar />
+      <TwoColumn
+        content={{
+          heading: "Omnus means everything",
+          body: "In Latin, “omnus” means “everything in one.” True to our name, we handle all aspects of updating or creating your website, including design, content writing, and development so you can stick to what you do best. ",
+        }}
+        cta={{
+          href: "/",
+          text: "Learn more about us",
+          variant: ButtonVariants.PRIMARY,
+        }}
+      />
       <Services />
     </Layout>
   );
